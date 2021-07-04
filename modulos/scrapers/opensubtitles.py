8617680423bf2adf7271a8ab26a68adee7e010e5
8XA <69772360+8XA@users.noreply.github.com>
2021-07-04 03:45:07 -0700

    segunda aproximación al scraper de opensubtitles, obtiene titulo, url y algunas veces descripcion, para series ahora es indistinto el orden de episodio-temporada

--- modulos/scrapers/opensubtitles.py
+++ modulos/scrapers/opensubtitles.py
@@ -11,28 +11,39 @@ import os
 #Ejemplo: S01E01
 def serie(palabra):
     palabra_min = palabra.lower()
-    caracteres = "se0123456789"
 
     #Condiciones para admitirlo como temporada/episodio
     if all(
             [
                 #Tiene solo caracteres admitidos
-                len([c for c in palabra_min if c in caracteres]) == len(palabra_min),
+                len([c for c in palabra_min if c in "se0123456789"]) == len(palabra_min),
 
-                #Tiene solo una S y es el primer caracter
-                (palabra_min[0] == "s") and (palabra_min.count("s") == 1),
+                #Tiene las dos letras y estas no se repiten
+                (len([c for c in palabra_min if c in "se"]) == 2) and (palabra_min.count("s") == 1),
 
-                #Tiene solo una E y no es el ultimo caracter
-                (palabra_min[-1] != "e") and (palabra_min.count("e") == 1),
+                #La inicial es una letra
+                palabra_min[0] in "se",
 
-                #Hay un número despues de cada letra
-                (len(palabra_min) > 3) and (palabra_min[1] != "e")
+                #Tiene mas de 3 caracteres y el segundo no es una letra
+                (len(palabra_min) > 3) and (palabra_min[1] not in "se"),
+
+                #El ultimo caracter no es una letra
+                palabra_min[-1] not in "se"
             ]
             ):
 
         #Separa el episodio y la temporada y los retorna en una lista
-        separacion = palabra_min.split("e")
-        return [int(separacion[0][1:]), int(separacion[1])]
+        letra_0 = palabra_min[0]
+        letra_1 = [c for c in palabra_min[1:] if c in "se"][0]
+        posicion_divisora = palabra_min.index(letra_1)
+
+        temp_cap = {
+                letra_0:palabra_min[1:posicion_divisora],
+                letra_1:palabra_min[posicion_divisora + 1:]
+                }
+
+        #Retorna [temporada, capítulo]
+        return [int(temp_cap["s"]), int(temp_cap["e"])]
 
     else:
         return False
@@ -45,8 +56,8 @@ def opensubtitles(palabras):
     temp, cap = "", ""
     if len(episodio) > 0:
         temp_cap = serie(episodio[0])
-        temp = "season-" + str(temp_cap[0]) + "/", 
-        cap = "episode-" + str(temp_cap[1]) + "/", 
+        temp = "season-" + str(temp_cap[0]) + "/"
+        cap = "episode-" + str(temp_cap[1]) + "/" 
 
     #Si es una serie, elimina las palabras relativas al episodio
     #(ejemplo: s01e01) y las pasa como parametro
@@ -68,12 +79,20 @@ def opensubtitles(palabras):
     linkBusqueda = "https://www.opensubtitles.org/es/search/sublanguageid-spa,spl/" + temp + cap + "moviename-" + "+".join(palabras_busqueda) + "/offset-" + str(pagina*40)
     txtBusqueda = os.popen("curl '" + linkBusqueda + "' | iconv -f iso-8859-1 -t utf-8").read()
 
+#    with open("prueba","w") as doc:
+#        doc.write(txtBusqueda)
+#    input()
+
     renglones = txtBusqueda.split("\n")
     indices = [x for x in range(len(renglones)) if " - Ver En l" in renglones[x]]
 
     subs = []
     #return [renglones,indices]
     for indice in indices:
+#        print()
+#        print(renglones[indice])
+#        print(len(subs))
+#        print()
         #Determinando enlace del subtítulo
         enlace_0 = renglones[indice+1].index("href=") + 6
         enlace_1 = renglones[indice+1].index("onclick") - 2
@@ -81,15 +100,22 @@ def opensubtitles(palabras):
 
         #Si la descripcion aparece completa
         if renglones[indice][0] == "(":
-            desfase_r, desfase_ad = 1, 0
             indexador_0, indexador_1, corrector = "<br />", "<br /><a", 6
+            desfase_t, desfase_a, desfase_d = 1, 0, 0
         #Si la descripción aparece incompleta
         else:
-            desfase_r, desfase_ad = 2, 1
             indexador_0, indexador_1, corrector = "<span title=", '">', 13
+            desfase_a, desfase_d = 0, 0
+            while renglones[indice - desfase_a][0] != "(":
+                desfase_a += 1
+
+            desfase_t = desfase_a + 1
+            
+            while "<span title=" not in renglones[indice - desfase_d]:
+                desfase_d += 1
 
         #Determinando título del subtítulo
-        titulo_1_des = [x for x in renglones[indice-desfase_r]]
+        titulo_1_des = [x for x in renglones[indice-desfase_t]]
         titulo_1_des.reverse()
         titulo_0 = "".join(titulo_1_des).index('>""=')
         titulo = titulo_1_des[:titulo_0]
@@ -97,16 +123,16 @@ def opensubtitles(palabras):
         titulo = "".join(titulo)
         
         #Determina el año
-        year_0 = renglones[indice-desfase_ad].index("</a")
-        year = renglones[indice-desfase_ad][:year_0]
+        year_0 = renglones[indice-desfase_a].index("</a")
+        year = renglones[indice-desfase_a][:year_0]
 
         #Título completo
         fulltitle = titulo + " " + year
 
         #Determinando descripción del subtítulo
-        descripcion_0 = renglones[indice-desfase_ad].index(indexador_0) + corrector
-        descripcion_1 = renglones[indice-desfase_ad][descripcion_0:].index(indexador_1)
-        descripcion = renglones[indice-desfase_ad][descripcion_0:descripcion_1 + descripcion_0]
+        descripcion_0 = renglones[indice-desfase_d].index(indexador_0) + corrector
+        descripcion_1 = renglones[indice-desfase_d][descripcion_0:].index(indexador_1)
+        descripcion = renglones[indice-desfase_d][descripcion_0:descripcion_1 + descripcion_0]
 
         subs.append([fulltitle, descripcion, enlace])
     
