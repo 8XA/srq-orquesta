af43a183c74da16ad2fc506dc48e0d76a8e5862a
8XA <69772360+8XA@users.noreply.github.com>
2021-07-05 01:27:03 -0700

    Auxiliar para obtener la url de descarga en subdivx, scrapers agregan -L en curl, opensubtitles integra temporizador, pantalla palabras admite 't' para seleccionar todas las palabras

---
+++ modulos/scrapers/auxiliares/subdivx/enlace_descarga.py
@@ -0,0 +1,24 @@
+#!/bin/env python
+
+#Recibe un enlace de descarga. Con él, 
+#retorna el enlace directo y el ID de archivo
+
+import os
+
+def descarga(enlace_de_lista):
+    #Descargando html
+    txtPagina = os.popen("curl '" + enlace_de_lista + "' | iconv -f iso-8859-1 -t utf-8").read()
+    
+    #Scraping
+    ind1 = txtPagina.index('"link1" href="bajar.php?id=')
+    ind2 = txtPagina[ind1+27:].index('&u=')
+    idsub = txtPagina[ind1+27:ind1+27+ind2]
+    u = txtPagina[30+ind1+ind2]
+    
+    #Generando link de descarga
+    num = ""
+    if u != "1":
+        num = u
+    link = "https://www.subdivx.com/sub" + num + "/" + idsub
+
+    return [link,idsub]
