004c4722776a7612637ab78de99ebb501f00c1cd
8XA <ochoa.ssa@gmail.com>
2021-07-04 17:00:45 -0700

    Separacion del depurador de caracteres de los scrapers, opensubtitles completo. Queda tentativo mejorar el comando curl para evitar bloqueos de opensubtitles

--- modulos/scrapers/opensubtitles.py
+++ modulos/scrapers/opensubtitles.py
@@ -6,6 +6,7 @@
 #[["titulo1", "descripcion1", "url1"], ["titulo2", "descripcion2", "url2"] ... ]
 
 import os
+from modulos.caracteres_raros import  corregir
 
 #Determina si la palabra es in indicador de una serie
 #Ejemplo: S01E01
@@ -49,91 +50,94 @@ def serie(palabra):
         return False
 
 
+###############################################
+
+#Scraper
 def opensubtitles(palabras):
-    #Determina si es una serie
-    episodio = [palabra for palabra in palabras if serie(palabra) != False]
+    try:
+        #Determina si es una serie
+        episodio = [palabra for palabra in palabras if serie(palabra) != False]
 
-    temp, cap = "", ""
-    if len(episodio) > 0:
-        temp_cap = serie(episodio[0])
-        temp = "season-" + str(temp_cap[0]) + "/"
-        cap = "episode-" + str(temp_cap[1]) + "/" 
+        temp, cap = "", ""
+        if len(episodio) > 0:
+            temp_cap = serie(episodio[0])
+            temp = "season-" + str(temp_cap[0]) + "/"
+            cap = "episode-" + str(temp_cap[1]) + "/" 
 
-    #Si es una serie, elimina las palabras relativas al episodio
-    #(ejemplo: s01e01) y las pasa como parametro
-    palabras_busqueda = [palabra for palabra in palabras if palabra not in episodio]
-    
-    pagina = 0
-
-#    lista = []
-#    txtBusqueda, pagina = "", 0
-#    while (pagina == 0) or (len(txtBusqueda) > 500):
-#        linkBusqueda = "https://www.opensubtitles.org/es/search/sublanguageid-spa,spl/" + temp + cap + "moviename-" + "+".join(palabras_busqueda) + "/offset-" + str(pagina*40)
-#        txtBusqueda = os.popen("curl '" + linkBusqueda + "' | iconv -f iso-8859-1 -t utf-8").read()
-#        lista.append(txtBusqueda)
-#        with open("scrap" + str(pagina), "w") as docu:
-#            docu.write(txtBusqueda)
-#        pagina += 1
-
-
-    linkBusqueda = "https://www.opensubtitles.org/es/search/sublanguageid-spa,spl/" + temp + cap + "moviename-" + "+".join(palabras_busqueda) + "/offset-" + str(pagina*40)
-    txtBusqueda = os.popen("curl '" + linkBusqueda + "' | iconv -f iso-8859-1 -t utf-8").read()
-
-#    with open("prueba","w") as doc:
-#        doc.write(txtBusqueda)
-#    input()
-
-    renglones = txtBusqueda.split("\n")
-    indices = [x for x in range(len(renglones)) if " - Ver En l" in renglones[x]]
-
-    subs = []
-    #return [renglones,indices]
-    for indice in indices:
-#        print()
-#        print(renglones[indice])
-#        print(len(subs))
-#        print()
-        #Determinando enlace del subtítulo
-        enlace_0 = renglones[indice+1].index("href=") + 6
-        enlace_1 = renglones[indice+1].index("onclick") - 2
-        enlace = "https://www.opensubtitles.org" + renglones[indice+1][enlace_0:enlace_1]
-
-        #Si la descripcion aparece completa
-        if renglones[indice][0] == "(":
-            indexador_0, indexador_1, corrector = "<br />", "<br /><a", 6
-            desfase_t, desfase_a, desfase_d = 1, 0, 0
-        #Si la descripción aparece incompleta
-        else:
-            indexador_0, indexador_1, corrector = "<span title=", '">', 13
-            desfase_a, desfase_d = 0, 0
-            while renglones[indice - desfase_a][0] != "(":
-                desfase_a += 1
-
-            desfase_t = desfase_a + 1
-            
-            while "<span title=" not in renglones[indice - desfase_d]:
-                desfase_d += 1
-
-        #Determinando título del subtítulo
-        titulo_1_des = [x for x in renglones[indice-desfase_t]]
-        titulo_1_des.reverse()
-        titulo_0 = "".join(titulo_1_des).index('>""=')
-        titulo = titulo_1_des[:titulo_0]
-        titulo.reverse()
-        titulo = "".join(titulo)
+        #Si es una serie, elimina las palabras relativas al episodio
+        #(ejemplo: s01e01) y las pasa como parametro
+        palabras_busqueda = [palabra for palabra in palabras if palabra not in episodio]
         
-        #Determina el año
-        year_0 = renglones[indice-desfase_a].index("</a")
-        year = renglones[indice-desfase_a][:year_0]
+        pagina, subs = 0, []
+        while (pagina == 0) or (len(txtBusqueda) > 1000):
 
-        #Título completo
-        fulltitle = titulo + " " + year
+            #Curl con headers
+            #curl = "curl -H 'authority: 25748s.ha.azioncdn.net' -H 'sec-ch-ua: " + '"Chromium";v="91", " Not;A Brand";v="99"' + "' -H 'sec-ch-ua-mobile: ?0' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36' -H 'accept: */*' -H 'origin: https://www.opensubtitles.org' -H 'sec-fetch-site: cross-site' -H 'sec-fetch-mode: cors' -H 'sec-fetch-dest: empty' -H 'referer: https://www.opensubtitles.org/' -H 'accept-language: es-ES,es;q=0.9' --compressed "
+            #Curl sencilla
+            curl = "curl "
+            linkBusqueda = "https://www.opensubtitles.org/es/search/sublanguageid-spa,spl/" + temp + cap + "moviename-" + "+".join(palabras_busqueda) + "/offset-" + str(pagina*40)
+            txtBusqueda = os.popen(curl + "'" + linkBusqueda + "' | iconv -f iso-8859-1 -t utf-8").read()
+            
+            #Sección para pruebas
+            with open("prueba","w") as documento:
+                documento.write(linkBusqueda)
+                #documento.write(txtBusqueda)
 
-        #Determinando descripción del subtítulo
-        descripcion_0 = renglones[indice-desfase_d].index(indexador_0) + corrector
-        descripcion_1 = renglones[indice-desfase_d][descripcion_0:].index(indexador_1)
-        descripcion = renglones[indice-desfase_d][descripcion_0:descripcion_1 + descripcion_0]
+            renglones = txtBusqueda.split("\n")
+            indices = [x for x in range(len(renglones)) if " - Ver En l" in renglones[x]]
 
-        subs.append([fulltitle, descripcion, enlace])
-    
-    return subs
+            for indice in indices:
+                #Determinando enlace del subtítulo
+                enlace_0 = renglones[indice+1].index("href=") + 6
+                enlace_1 = renglones[indice+1].index("onclick") - 2
+                enlace = "https://www.opensubtitles.org" + renglones[indice+1][enlace_0:enlace_1]
+
+                #Si la descripcion aparece completa
+                if renglones[indice][0] == "(":
+                    indexador_0, indexador_1, corrector = "<br />", "<br /><a", 6
+                    desfase_t, desfase_a, desfase_d = 1, 0, 0
+                #Si la descripción aparece incompleta
+                else:
+                    indexador_0, indexador_1, corrector = "<span title=", '">', 13
+                    desfase_a, desfase_d = 0, 0
+                    while renglones[indice - desfase_a][0] != "(":
+                        desfase_a += 1
+
+                    desfase_t = desfase_a + 1
+                    
+                    while "<span title=" not in renglones[indice - desfase_d]:
+                        desfase_d += 1
+
+                #Determinando título del subtítulo
+                titulo_1_des = [x for x in renglones[indice-desfase_t]]
+                titulo_1_des.reverse()
+                titulo_0 = "".join(titulo_1_des).index('>""=')
+                titulo = titulo_1_des[:titulo_0]
+                titulo.reverse()
+                titulo = "".join(titulo)
+                
+                #Determina el año
+                year_0 = renglones[indice-desfase_a].index("</a")
+                year = renglones[indice-desfase_a][:year_0]
+
+                #Título completo
+                fulltitle = corregir(titulo + " " + year)
+
+                #Determinando descripción del subtítulo
+                if (renglones[indice][0] == "(") or (renglones[indice-desfase_d][-3:] == "..."):
+                    descripcion_0 = renglones[indice-desfase_d].index(indexador_0) + corrector
+                    descripcion_1 = renglones[indice-desfase_d][descripcion_0:].index(indexador_1)
+                    descripcion = corregir(renglones[indice-desfase_d][descripcion_0:descripcion_1 + descripcion_0])
+                elif renglones[indice][0] not in "(<":
+                    descripcion = corregir(renglones[indice-1] + " " + renglones[indice][:renglones[indice].index("<br /><a")])
+                else:
+                    descripcion = ""
+
+                subs.append([fulltitle, descripcion, enlace])
+
+            pagina += 1
+
+        return subs
+
+    except:
+        return []
--- modulos/scrapers/subdivx.py
+++ modulos/scrapers/subdivx.py
@@ -6,53 +6,43 @@
 #[["titulo1", "descripcion1", "url1"], ["titulo2", "descripcion2", "url2"] ... ]
 
 import os
+from modulos.caracteres_raros import corregir
+
 def subdivx(palabras):
-    subs, subspag, pagina = [], [], 0
-    while subspag != [] or pagina == 0:
-        pagina+=1
+    try:
+        subs, subspag, pagina = [], [], 0
+        while subspag != [] or pagina == 0:
+            pagina+=1
 
-        #Obtiene el html del link con las palabras de búsqueda
-        suma = "+".join(palabras)
-        linkBusqueda = "https://www.subdivx.com/index.php?buscar=" + suma + "&accion=5&masdesc=&subtitulos=1&realiza_b=1=&pg=" + str(pagina)
-        txtBusqueda = os.popen("curl '" + linkBusqueda + "' | iconv -f iso-8859-1 -t utf-8").read()
+            #Obtiene el html del link con las palabras de búsqueda
+            suma = "+".join(palabras)
+            linkBusqueda = "https://www.subdivx.com/index.php?buscar=" + suma + "&accion=5&masdesc=&subtitulos=1&realiza_b=1=&pg=" + str(pagina)
+            txtBusqueda = os.popen("curl '" + linkBusqueda + "' | iconv -f iso-8859-1 -t utf-8").read()
 
-        #Extrae la información del html descargado
-        x = 0
-        while '"titulo_menu_izq" href="' in txtBusqueda[x:]:
-            ind = txtBusqueda[x:].index('"titulo_menu_izq" href="')
-            ind2 = txtBusqueda[x+ind:].index('">')
-            ind3 = txtBusqueda[x+ind+ind2:].index('</a>')
+            #Extrae la información del html descargado
+            x = 0
+            while '"titulo_menu_izq" href="' in txtBusqueda[x:]:
+                ind = txtBusqueda[x:].index('"titulo_menu_izq" href="')
+                ind2 = txtBusqueda[x+ind:].index('">')
+                ind3 = txtBusqueda[x+ind+ind2:].index('</a>')
 
-            enlace = txtBusqueda[24+x+ind:x+ind+ind2]
-            titulo = txtBusqueda[x+ind+ind2+2:x+ind+ind2+ind3]
-            
-            x += (ind+ind2+ind3)
-            ind4 = txtBusqueda[x:].index('<div id="buscador_detalle_sub"')
-            ind5 = txtBusqueda[x+ind4:].index('</div>')
+                #Título
+                enlace = txtBusqueda[24+x+ind:x+ind+ind2]
+                titulo = corregir(txtBusqueda[x+ind+ind2+2:x+ind+ind2+ind3])
+                
+                #Descripción
+                x += (ind+ind2+ind3)
+                ind4 = txtBusqueda[x:].index('<div id="buscador_detalle_sub"')
+                ind5 = txtBusqueda[x+ind4:].index('</div>')
+                descripcion = corregir(txtBusqueda[31+x+ind4:x+ind4+ind5])
 
-            descripcion = txtBusqueda[31+x+ind4:x+ind4+ind5]
-            
-            descripcion = "".join(descripcion.split("Â"))
-            descripcion = " ".join(descripcion.split("\n"))
-            descripcion = "ñ".join(descripcion.split("Ã±"))
-            descripcion = "á".join(descripcion.split("Ã¡"))
-            descripcion = "é".join(descripcion.split("Ã©"))
-            descripcion = "í".join(descripcion.split("Ã\xad"))
-            descripcion = "ó".join(descripcion.split("Ã³"))
-            descripcion = "ú".join(descripcion.split("Ãº"))
+                subspag.append([titulo, descripcion, enlace])
+                subs += subspag
+                subspag = []
 
-            titulo = "".join(titulo.split("Â"))
-            titulo = " ".join(titulo.split("\n"))
-            titulo = "ñ".join(titulo.split("Ã±"))
-            titulo = "á".join(titulo.split("Ã¡"))
-            titulo = "é".join(titulo.split("Ã©"))
-            titulo = "í".join(titulo.split("Ã\xad"))
-            titulo = "ó".join(titulo.split("Ã³"))
+                x += (ind4+ind5)
 
-            subspag.append([titulo, descripcion, enlace])
-            subs += subspag
-            subspag = []
+        return subs
 
-            x += (ind4+ind5)
-
-    return subs
+    except:
+        return []
