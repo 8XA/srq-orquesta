2b913d1b5bb19c7dbb00ff2a94879e2df5663a1f
8XA <69772360+8XA@users.noreply.github.com>
2021-07-07 13:28:27 -0700

    primera aproximación Descarga

--- pantallas/configuracion.py
+++ pantallas/configuracion.py
@@ -2,3 +2,17 @@
 
 def configuracion():
     pass
+
+#Numero de resultados por pagina
+#IDs descargables:
+#    por pagina
+#    por filtro
+#    por disponibles
+#inicio automatico
+#buscar actualizaciones al inicio
+#Buscar actualizaciones ahora
+#recodificar
+#peliculas y carpetas en una linea
+#formatos de video aceptados
+#scrapers
+#
