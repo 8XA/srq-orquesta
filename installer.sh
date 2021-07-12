ff1bbacb5ecd0fdfee9915613900e81854729f3e
8XA <69772360+8XA@users.noreply.github.com>
2021-07-12 14:12:58 -0700

    Instalador integra dependencia 'file'

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ pkg install -y iconv
 pkg install -y unrar
 pkg install -y p7zip
 pkg install -y readline
-#pkg install -y file
+pkg install -y file
 pip install termcolor
 pip install chardet
 echo "alias sub='exec python /data/data/com.termux/files/usr/share/sub4time/sub4time/start.py'" >> "/data/data/com.termux/files/usr/etc/bash.bashrc"
@@ -15,6 +15,3 @@ clear
 termux-setup-storage
 echo "Instalación completa. Presiona Enter para salir e inicia Termux de nuevo."
 read listo
-
-
-/data/data/com.termux/files/usr/etc/bash.bashrc
