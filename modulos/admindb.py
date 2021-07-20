9f03e457851b033b4d64c07fee3a309d307ba8d2
8XA <ochoa.ssa@gmail.com>
2021-07-20 13:13:18 -0700

    Configuraciones se mantienen tras actualizar

--- modulos/admindb.py
+++ modulos/admindb.py
@@ -1,8 +1,9 @@
 #!/bin/env python
 
-import sqlite3
+import sqlite3, os
 
 ruta = "/data/data/com.termux/files/usr/share/sub4time/sub4time/data.db"
+ruta_backup = "/data/data/com.termux/files/usr/share/sub4time/data_backup.db"
 
 #SETTINGS
 
@@ -17,25 +18,34 @@ def editar_settings(columna, nuevo_valor):
 
 #Lee en tabla settings
 #Recibe el nombre de la columna como parámetro y retorna su valor
-def leer_settings(columna):
-    global ruta
+def leer_settings(*args):
+    global ruta, ruta_backup
+    columna = args[0]
+    
     columnas = ["actualizar", "ini_aut", "ruta_carpeta", "ruta_video","video",
             "palabras", "scrapers", "recode", "menu", "menu_anterior", "oneline", 
             "extensiones", "id_descargable", "cambio_busqueda", "link_descarga",
             "subs_descargados", "rpp", "filtro_videos", "instancia_activa"]
 
-    conexion = sqlite3.connect(ruta)
+    data = ruta
+    if (len(args) > 1) and (args[1] == "backup"):
+        data = ruta_backup
+
+    conexion = sqlite3.connect(data)
     cursor = conexion.cursor()
     cursor.execute("SELECT * FROM settings")
+
+    if (len(args) > 1) and (args[1] == "backup"):
+        columnas_backup = [descripcion[0] for descripcion in cursor.description]
 
     linea_1 = cursor.fetchone()
     conexion.close()
 
+    #Si es lectura de backup, retorna tupla con lista de columnas,
+    #y tupla de valores
+    if (len(args)) > 1 and (args[1] == "backup"):
+        return columnas_backup, linea_1
     return linea_1[columnas.index(columna)]
-
-#Restaurar backup
-def restaurar_settings():
-    pass
 
 
 #RESULTADOS
@@ -53,9 +63,14 @@ def editar_resultados(lista_subs):
     conexion.close()
 
 #Lee en tabla resultados y retorna una lista de tuplas
-def leer_resultados():
-    global ruta
-    conexion = sqlite3.connect(ruta)
+def leer_resultados(*args):
+    global ruta, ruta_backup
+    
+    data = ruta
+    if (len(args) == 1) and (args[0] == "backup"):
+        data = ruta_backup
+
+    conexion = sqlite3.connect(data)
     cursor = conexion.cursor()
     cursor.execute("SELECT * FROM resultados")
 
@@ -63,3 +78,20 @@ def leer_resultados():
     conexion.close()
 
     return subs
+
+
+#RESTAURAR BACKUP
+def restaurar_settings():
+    global ruta_backup 
+
+    if os.path.isfile(ruta_backup):
+        settings = leer_settings("","backup")
+
+        for x in range(len(settings[0])):
+            editar_settings(settings[0][x], str(settings[1][x]))
+        
+        subs = leer_resultados("backup")
+        editar_resultados(subs)
+    
+    os.system("rm " + ruta_backup)
+
