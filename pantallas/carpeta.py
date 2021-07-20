7c45af9864ca21749d43e64bb55dd7d27781ad07
8XA <69772360+8XA@users.noreply.github.com>
2021-07-20 15:33:42 -0700

    Seccón 'Carpeta admite filtrado'

--- pantallas/carpeta.py
+++ pantallas/carpeta.py
@@ -8,7 +8,7 @@ from termcolor import colored
 from modulos.admindb import leer_settings, editar_settings
 from modulos.numcols import num_cols
 from modulos.menu import menu
-from modulos.fit_frases import fit_frase
+from modulos.fit_frases import *
 
 def carpeta():
     editar_settings("menu_anterior", str(leer_settings("menu")))
@@ -17,8 +17,8 @@ def carpeta():
     ruta = leer_settings("ruta_carpeta")
     cadena = [carpeta for carpeta in ruta.split("/") if carpeta != ""]
 
-    accion = "."
-    while accion != "":
+    filtros_str = " "
+    while filtros_str != "":
         numcols = num_cols()
         linea_azul = colored(numcols*"=", 'blue', attrs=['bold', 'dark'])
         linea_roja = colored(numcols*"=", 'red', attrs=['bold', 'dark'])
@@ -33,14 +33,19 @@ def carpeta():
         carpetas = sorted([carpeta.path.split("/")[-1:][0] for \
                 carpeta in os.scandir(ruta) if carpeta.is_dir()])
 
+        filtros_str = " ".join(filtros_str.lower().split(","))
+        filtros = filtros_str.split(" ")
         for x in range(len(carpetas)):
-            indice = colored(str(x), 'green', attrs=['bold', 'dark'])
+            if len([filtro for filtro in filtros if filtro in \
+                    carpetas[x].lower()]) == len(filtros):
 
-            #Recorta el renglon si está activado "oneline" en settings
-            if leer_settings("oneline") == 1:
-                print(indice + ":",carpetas[x][:numcols - len(str(x)) - 2])
-            else:
-                print(indice + ":",carpetas[x])
+                indice = colored(str(x), 'green', attrs=['bold', 'dark'])
+
+                #Recorta el renglon si está activado "oneline" en settings
+                if leer_settings("oneline") == 1:
+                    print(indice + ":",carpetas[x][:numcols - len(str(x)) - 2])
+                else:
+                    print(indice + ":",carpetas[x])
 
         if len(carpetas) == 0:
             msj = "Fin de la ruta, no hay más carpetas adelante..."
@@ -52,7 +57,12 @@ def carpeta():
 
         #Ruta actual en franja roja
         print(linea_roja)
+        print(colored(fit_frase_centrada(numcols, "Ruta:"), 'white', attrs=['bold']))
         print(ruta)
+        print(linea_roja)
+        #Filtros
+        print(colored(fit_frase_centrada(numcols, "Filtros:"), 'white', attrs=['bold']))
+        print(fit_frase_centrada(numcols, filtros_str))
         print(linea_roja)
 
         i = menu(numcols, "Define la carpeta de búsqueda")
@@ -61,19 +71,19 @@ def carpeta():
         if i[0] == "menu":
             return i[1]
         else:
-            accion = i[1]
-            if accion == "":
+            if i[1] == "":
                 editar_settings("ruta_carpeta", ruta)
                 return leer_settings("menu_anterior")
 
-            elif "".join([x for x in accion if x in "0123456789"]) == accion:
-                if len(carpetas) > int(accion) >= 0:
-                    cadena.append(carpetas[int(accion)])
-                    ruta = "/" + "/".join(cadena) + "/"
+            elif (i[1].isdigit()) and (len(carpetas) > int(i[1])):
+                cadena.append(carpetas[int(i[1])])
+                ruta = "/" + "/".join(cadena) + "/"
+                filtros_str = " "
 
-            elif accion == "..":
+            elif i[1] == "..":
                 if len(cadena) > 1:
                     cadena.pop()
+                    filtros_str = " "
                 ruta = "/" + "/".join(cadena) + "/"
             else:
-                pass
+                filtros_str = i[1]
