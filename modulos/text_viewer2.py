fdf29e6ab30dd51c1a3d1748d004668bd2b10cd2
8XA <ochoa.ssa@gmail.com>
2021-07-23 14:49:07 -0700

    Aproximacion de codigos para manual Carpeta

--- modulos/text_viewer2.py
+++ modulos/text_viewer2.py
@@ -1,12 +1,11 @@
 #!/bin/env python
 
-import os
-from termcolor import colored
-from modulos.menu import menu
+import os, curses
 from modulos.fit_frases import *
 
-
-def visor(titulo, archivo):
+def visor(*arg):
+    #arg[0] = titulo
+    #arg[1...10] = archivo1... archivo10
     ruta = "/data/data/com.termux/files/usr/share/sub4time/sub4time/imprimibles/"
 
     screen = curses.initscr()
@@ -16,6 +15,24 @@ def visor(titulo, archivo):
     screen.keypad(True)
     #Habilitar configuración de colores
     curses.start_color()
+
+    #Dimensiones de pantalla
+    largo_pantalla = curses.LINES
+    ancho_pantalla = curses.COLS
+
+    #Lista de archivos
+    #Cada archivo es una lista de renglones
+    archivos_lista = []
+    for archivo in arg[1:]:
+        with open(ruta + archivo, "r") as archivo_texto:
+            archivos_lista.append(archivo_texto.readlines())
+
+    #Prueba imprimiendo primer archivo
+    for renglon in archivos_lista[0]:
+        print(renglon[4:])
+
+    input()
+
 
     #Título
     #Ventana scroll con texto:
@@ -38,7 +55,7 @@ def visor(titulo, archivo):
     
     #Linea de final y leyenda de Enter para regresar
 
-    return pantalla
+    return largo_pantalla,ancho_pantalla
 
 #abres archivo de texto
 #lista split saltos de renglon
