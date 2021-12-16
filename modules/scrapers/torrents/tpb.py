265605284ff08e42a29b4c87b314d4df16d81009
8XA <ochoa.ssa@gmail.com>
2021-12-15 22:51:03 -0700

    Fix: tpb module get all the video categories

--- modules/scrapers/torrents/tpb.py
+++ modules/scrapers/torrents/tpb.py
@@ -5,11 +5,10 @@ from tpblite import TPB, ORDERS, CATEGORIES
 def tpb(search):
 
     # TPB object with the default domain
-    t = TPB('https://tpb.party')
+    t = TPB()
 
     # Getting the data
-    torrents = t.search(search, order=ORDERS.SEEDERS.DES, \
-            category=CATEGORIES.VIDEO.MOVIES)
+    torrents = t.search(search, order=ORDERS.SEEDERS.DES, category=CATEGORIES.VIDEO.ALL)
 
     torrent_list = [[torrent.title, torrent.filesize, torrent.seeds, \
             torrent.leeches, "TPB", torrent.magnetlink] \
