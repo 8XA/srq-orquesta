93a61979fb4e5292438fa15db4ac3ac7a10d6c9f
8XA <ochoa.ssa@gmail.com>
2021-12-16 12:23:46 -0700

    Fix: spanish vowels for strange_characters module are complete

--- modules/strange_characters.py
+++ modules/strange_characters.py
@@ -7,11 +7,15 @@ def corregir(texto_original):
     texto = "ñ".join(texto.split("Ã±"))
     texto = "Ñ".join(texto.split("Ã\x91"))
     texto = "á".join(texto.split("Ã¡"))
+    texto = "Á".join(texto.split("Ã\x81"))
     texto = "é".join(texto.split("Ã©"))
+    texto = "É".join(texto.split("Ã\x89"))
     texto = "í".join(texto.split("Ã\xad"))
+    texto = "Í".join(texto.split("Ã\x8d"))
     texto = "ó".join(texto.split("Ã³"))
     texto = "Ó".join(texto.split("Ã\x93"))
     texto = "ú".join(texto.split("Ãº"))
+    texto = "Ú".join(texto.split("Ã\x9a"))
     texto = "'".join(texto.split("â\x80\x99"))
 
     return texto
