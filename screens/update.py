c5d2206babae3a2853598c05fbb24dab1a511774
8XA <ochoa.ssa@gmail.com>
2021-12-17 12:06:19 -0700

    refactor: english vars for update. 'visor' function is 'viewer' now

--- screens/update.py
+++ screens/update.py
@@ -1,41 +1,41 @@
 #!/bin/env python
 
-import os, time
-#from modules.columns_number import columns_number_func
-from modules.viewer import visor
+from time import sleep
+from os import system, popen
+from modules.viewer import viewer
 
 def update():
-    rabs = '/data/data/com.termux/files/usr/share/srq-orquesta/srq-orquesta/'
-    ruta_backup = "/data/data/com.termux/files/usr/share/srq-orquesta/data_backup.db"
+    absolute_route = '/data/data/com.termux/files/usr/share/srq-orquesta/srq-orquesta/'
+    data_backup_route = "/data/data/com.termux/files/usr/share/srq-orquesta/data_backup.db"
 
-    os.system("clear")
+    system("clear")
     print("Verificando actualizaciones para SRQ ORQUESTA...\n")
 
-    remoto = os.popen('cd '+ rabs + ' && git fetch origin master ' + \
+    remote = popen('cd '+ absolute_route + ' && git fetch origin master ' + \
             '&& git checkout remotes/origin/master').read()
-    local = os.popen('cd ' + rabs + ' && git checkout master').read()
+    local = popen('cd ' + absolute_route + ' && git checkout master').read()
     
-    #Verifica que haya alguna actualización en el repo
+    # Verifies if there is an update in the repositorie
     if "commit" in local:
         print("Actualizando script...\n")
-        os.system('rm -rf update')
-        clonar = os.system('git clone --branch master --single-branch ' + \
+        system('rm -rf update')
+        clonar = system('git clone --branch master --single-branch ' + \
                 'https://github.com/8XA/srq-orquesta.git update')
 
-        #Si la descarga se realizó con exito, hace la actualización
+        # If the download was successful, it executes the update
         if clonar == 0:
-            os.system ("cp " + rabs + "data.db " + ruta_backup)
-            os.system('rm -rf ' + rabs[:-1])
-            os.system('mv update ' + rabs[:-1] + ' && clear')
+            system ("cp " + absolute_route + "data.db " + data_backup_route)
+            system('rm -rf ' + absolute_route[:-1])
+            system('mv update ' + absolute_route[:-1] + ' && clear')
             
-            visor("ACTUALIZACIÓN COMPLETA", "update")
+            viewer("ACTUALIZACIÓN COMPLETA", "update")
             input("Enter para salir...")
 
             return 'exit_srq'
 
     else:
-        os.system("clear")
+        system("clear")
         print("Ya tienes la última versión de SRQ ORQUESTA. Nada para hacer...")
-        time.sleep(1)
-        os.system("clear")
+        sleep(1)
+        system("clear")
         return 'videos'
