61f45d71eb129ae30717ab1dfb7aed6c34e6a4e7
8XA <69772360+8XA@users.noreply.github.com>
2022-01-08 17:09:39 -0700

    Fix: opensubtitles scraper. Fix: Updating with new dependencies.

---
+++ modules/dependencies_verify.py
@@ -0,0 +1,77 @@
+#!/bin/env python
+
+from os import system, popen
+from os.path import isfile
+from sys import exit
+
+def verify():
+    """
+    Verify the installed dependencies and install those are missing.
+    """
+
+    #It runs only on termux
+    if not isfile("/data/data/com.termux/files/usr/bin/termux-info"):
+        return None
+    
+    #PKG verify and installation
+    pkg_dependencies = [
+            "wget",
+            "unrar",
+            "p7zip",
+            "readline",
+            "file",
+            "libxslt" # tpblite dependency
+        ]
+
+    #Gets the installed packages with apt
+    raw_apt = popen("apt list --installed").read()
+    split_apt = raw_apt.split("\n")
+    apt_list = [pkg[:pkg.index("/")] for pkg in split_apt if "/" in pkg]
+
+    #List of the missing packages
+    pkg_to_install = [to_install for to_install in pkg_dependencies if to_install not in apt_list]
+
+    #Upgrading system packages
+    if len(pkg_to_install) > 0:
+        system("apt-get update")
+        system("apt-get upgrade - y")
+
+    #Install the missing packages
+    for pkg in pkg_to_install:
+        system("pkg install -y " + pkg)
+
+    #PIP verify and installation
+    pip_dependencies = [
+            "tpblite",
+            "termcolor",
+            "chardet",
+            "requests"
+        ]
+
+    #Backup of the pip freeze file
+    freeze_route = '/data/data/com.termux/files/usr/share/srq-orquesta/pip_freeze.txt'
+    if not isfile(freeze_route):
+        system("pip freeze > " + freeze_route)
+
+    #Gets the pip list from the backup pip file
+    with open(freeze_route, "r") as file:
+        raw_pip = file.readlines()
+    pip_list = [pkg[:pkg.index("=")] for pkg in raw_pip]
+
+    #List of the missing packages
+    pip_to_install = [to_install for to_install in pip_dependencies if to_install not in pip_list]
+
+    #Upgrade pip
+    if len(pip_to_install) > 0:
+        system("pip install --upgrade pip")
+    #Install the missing packages
+    for pkg in pip_to_install:
+        system("pip install " + pkg)
+    #Updates the pip file
+    if len(pip_to_install) > 0:
+        system("pip freeze > " + freeze_route)
+
+    if len(pip_to_install) + len(pkg_to_install) > 0:
+        system("clear")
+        input("Dependencias actualizadas. Reinicia SRQ ORQUESTA. Enter: ")
+        exit()
