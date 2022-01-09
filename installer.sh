f892a648b936f50adef191e7cbd6c8704eacfa37
8XA <ochoa.ssa@gmail.com>
2022-01-08 17:45:19 -0700

    Fix: Installer: Commands for install external apps and the first SRQ ORQUESTA execution.

--- installer.sh
+++ installer.sh
@@ -3,16 +3,19 @@
 set -e
 pkg install -y python
 
+
 sed -i '1i exec python /data/data/com.termux/files/usr/share/srq-orquesta/srq-orquesta/start.py' /data/data/com.termux/files/usr/etc/bash.bashrc
 echo "alias srq='exec python /data/data/com.termux/files/usr/share/srq-orquesta/srq-orquesta/start.py'" >> "/data/data/com.termux/files/usr/etc/bash.bashrc"
 clear
 
-read SRQ ORQUESTA recomienda 'LibreTorrent' como cliente de descargas (es libre y soporta la descarga secuencial). Aunque puedes usar cualquier otro. Presiona Enter para abrirlo en la Play Store: 
+echo "SRQ ORQUESTA recomienda 'LibreTorrent' como cliente de descargas (es libre y soporta la descarga secuencial). Aunque puedes usar cualquier otro. Presiona Enter para abrirlo en la Play Store: "
+read
 xdg-open "https://play.google.com/store/apps/details?id=org.proninyaroslav.libretorrent"
 
 echo
 
-read SRQ ORQUESTA recomienda 'mpv-android' como reproductor de videos (es libre, integra códecs y soporta subtítulos). Aunque puedes usar cualquier otro. Presiona Enter para abrirlo en la Play Store:
+echo "SRQ ORQUESTA recomienda 'mpv-android' como reproductor de videos (es libre, integra códecs y soporta subtítulos). Aunque puedes usar cualquier otro. Presiona Enter para abrirlo en la Play Store: "
+read
 xdg-open "https://play.google.com/store/apps/details?id=is.xyz.mpv"
 
 clear
