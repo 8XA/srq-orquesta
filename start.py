39741ec3b3476a8550b8c884be671a584b8159b9
8XA <ochoa.ssa@gmail.com>
2022-01-08 17:26:13 -0700

    Fix: Reset of settings if there is an error.

--- start.py
+++ start.py
@@ -2,32 +2,33 @@
 
 from modules.dependencies_verify import verify
 verify()
-from modules.create_db import create_db
-create_db()
-from modules.admin_db import read_settings, edit_settings, restore_settings
-restore_settings()
-
-import readline
-from os import system
-from screens.update import update
-from screens.torrents import torrents
-from screens.videos import videos
-from screens.folder import folder
-from screens.words import words
-from screens.results import results
-from screens.settings import settings
-from screens.help import help_section
-from screens.about import about
-from screens.download import download
-from screens.onesession import one_session
-from modules.storage_verify import *
-from modules.columns_number import columns_number_func
-from modules.refresh_history import clean_history_tables
-from modules.strings_fitting import phrase_fitting
-
-columns_num = columns_number_func()
 
 try:
+    from modules.create_db import create_db
+    create_db()
+    from modules.admin_db import read_settings, edit_settings, restore_settings
+    restore_settings()
+
+    import readline
+    from os import system
+    from screens.update import update
+    from screens.torrents import torrents
+    from screens.videos import videos
+    from screens.folder import folder
+    from screens.words import words
+    from screens.results import results
+    from screens.settings import settings
+    from screens.help import help_section
+    from screens.about import about
+    from screens.download import download
+    from screens.onesession import one_session
+    from modules.storage_verify import *
+    from modules.columns_number import columns_number_func
+    from modules.refresh_history import clean_history_tables
+    from modules.strings_fitting import phrase_fitting
+
+    columns_num = columns_number_func()
+
     if read_settings("active_instance") == 0:
         edit_settings("active_instance", "1")
 
@@ -44,19 +45,6 @@ try:
             'download': download,
             'update': update,
         }
-    #
-    #    srq_orquesta = {
-    #        0: torrents,
-    #        1: videos,
-    #        2: folder,
-    #        3: words,
-    #        4: results,
-    #        5: settings,
-    #        6: help_section,
-    #        7: about,
-    #        101: download,
-    #        102: update,
-    #
 
         #This is a loop function. It verifies the access to the storage.
         storage_verify()
