e4b876907b3dd1dd626982ea92f2e9b52b1fc4b1
8XA <69772360+8XA@users.noreply.github.com>
2022-01-17 12:56:05 -0700

    Fix: folders browser avoids folders without permission

--- modules/files_from_route.py
+++ modules/files_from_route.py
@@ -3,25 +3,28 @@
 #Lista de los archivos + lista de sus rutas mostrados
 
 from modules.admin_db import read_settings
+from subprocess import Popen, PIPE
 import os
 
-#args[0]: extensiones separadas por coma
-#args[1]: ruta escaneada
-def archivos_en_ruta(*args):
-    extensiones = args[0].split(",")
+#Extensions: extensiones separadas por coma
+#Route: ruta escaneada
+def archivos_en_ruta(extensions:str, route:str):
+    extensiones = extensions.split(",")
     archivos = []
 
     for ext in extensiones:
-        archivos += os.popen("find '" + args[1] + \
-                "' -iname '*." + ext + "'").read().split("\n")
+        ext_files = Popen("find '" + route + "' -iname '*." + ext + "'", \
+                shell=True, stdout=PIPE, stderr=PIPE)
+        archivos += str(ext_files.stdout.read()).split("\\n")
     archivos = [archivo for archivo in archivos if archivo != ""]
     
     #Lista depurada con los nombres de los archivos
     lista_rutas, lista_archivos = [], []
     for x in archivos:
-        indice = x.rindex("/")
-        lista_rutas += [x[:indice + 1]]
-        lista_archivos += [x[indice + 1:]]
+        if '/' in x:
+            indice = x.rindex("/")
+            lista_rutas += [x[:indice + 1]]
+            lista_archivos += [x[indice + 1:]]
 
     return [lista_rutas, lista_archivos]
 
