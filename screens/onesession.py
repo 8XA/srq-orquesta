54e0b09c4f1aa97eae27dab4b9d9dd199c904ce7
8XA <69772360+8XA@users.noreply.github.com>
2022-02-02 00:12:43 -0700

    style: Red color for OneSession. OneSession requires just Enter
    style: Red color for OneSession. OneSession requires just Enter

--- screens/onesession.py
+++ screens/onesession.py
@@ -12,24 +12,24 @@ from time import sleep
 def one_session():
     cols_number = columns_number_func()
 
-    blue_line_ = colored(cols_number*"-", 'blue', attrs=['bold', 'dark'])
+    red_line_ = colored(cols_number*"-", 'red', attrs=['bold', 'dark'])
     blue_line = colored(cols_number*"=", 'blue', attrs=['bold', 'dark'])
-    green_zero = colored("0", 'green', attrs=['bold','dark'])
-    green_one = colored("1", 'green', attrs=['bold','dark'])
+    green_enter = colored("Enter", 'green', attrs=['bold','dark'])
+    green_n = colored("N", 'green', attrs=['bold','dark'])
 
     print(blue_line)
     message = "SRQ ORQUESTA está actualmente en ejecución o no se cerró correctamente. " + \
             "Tener más de una instancia abierta puede provocar un comportamiento inesperado."
 
-    options = green_one + ": Sí\n" + green_zero + ": No\n"
+    options = green_enter + ": Sí\n" + green_n + ": No\n"
     print(phrase_fitting(cols_number, message))
-    print(blue_line_)
+    print(red_line_)
     print(colored(centered_phrase_fitting(cols_number, "Desea forzar inicio?"), 'white', attrs=['bold']))
-    print(blue_line_)
+    print(red_line_)
     printable = options + blue_line + "\n: "
     i = input(printable)
 
-    if i.isdigit() and int(i) == 1:
+    if i == "":
         edit_settings("active_instance", "0")
         
         print("\nForzando apertura...")
