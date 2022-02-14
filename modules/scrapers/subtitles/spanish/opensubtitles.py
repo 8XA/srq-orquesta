c750957e6171b38296bdd16094cb79b9fba39353
8XA <ochoa.ssa@gmail.com>
2022-02-13 20:49:44 -0700

    Fix: Opensubtitles scraps all pages by using different user agents

--- modules/scrapers/subtitles/spanish/opensubtitles.py
+++ modules/scrapers/subtitles/spanish/opensubtitles.py
@@ -5,85 +5,36 @@
 #opensubtitles. El retorno tiene el formato:
 #[["titulo1", "descripcion1", "url1"], ["titulo2", "descripcion2", "url2"] ... ]
 
-import os, time
-from requests import get
+import os
+from requests import get, Session
+from modules.user_agents import user_agents
 from requests.exceptions import RequestException
 from urllib.parse import quote
 
-#Determina si la palabra es in indicador de una serie
-#Ejemplo: S01E01
-def serie(palabra):
-    palabra_min = palabra.lower()
-
-    #Condiciones para admitirlo como temporada/episodio
-    if all(
-            [
-                #Tiene solo caracteres admitidos
-                len([c for c in palabra_min if c in "se0123456789"]) == len(palabra_min),
-
-                #Tiene las dos letras y estas no se repiten
-                (len([c for c in palabra_min if c in "se"]) == 2) and (palabra_min.count("s") == 1),
-
-                #La inicial es una letra
-                palabra_min[0] in "se",
-
-                #Tiene mas de 3 caracteres y el segundo no es una letra
-                (len(palabra_min) > 3) and (palabra_min[1] not in "se"),
-
-                #El ultimo caracter no es una letra
-                palabra_min[-1] not in "se"
-            ]
-            ):
-
-        #Separa el episodio y la temporada y los retorna en una lista
-        letra_0 = palabra_min[0]
-        letra_1 = [c for c in palabra_min[1:] if c in "se"][0]
-        posicion_divisora = palabra_min.index(letra_1)
-
-        temp_cap = {
-                letra_0:palabra_min[1:posicion_divisora],
-                letra_1:palabra_min[posicion_divisora + 1:]
-                }
-
-        #Retorna [temporada, capítulo]
-        return [int(temp_cap["s"]), int(temp_cap["e"])]
-
-    else:
-        return False
-
-
-###############################################
-
-#Scraper
 def opensubtitles(palabras):
     try:
-        #Determina si es una serie
-        episodio = [palabra for palabra in palabras if serie(palabra) != False]
-
-        temp, cap = "", ""
-        if len(episodio) > 0:
-            temp_cap = serie(episodio[0])
-            temp = "season-" + str(temp_cap[0]) + "/"
-            cap = "episode-" + str(temp_cap[1]) + "/" 
-
-        #Si es una serie, elimina las palabras relativas al episodio
-        #(ejemplo: s01e01) y las pasa como parametro
-        palabras_busqueda = [quote(palabra) for palabra in palabras if palabra not in episodio]
+        palabras_busqueda = [quote(palabra) for palabra in palabras]
+        agents = user_agents()
         
         pagina, subs = 0, []
+        to_iterate = True
         #Solo descargará la primera página para evitar bloqueo
-        while pagina == 0:
+        while to_iterate:
 
             linkBusqueda = "https://www.opensubtitles.org/es/search/sublanguageid-spa,spl/" + \
-                    temp + cap + "moviename-" + "+".join(palabras_busqueda) + "/offset-" + str(pagina*40)
+                    "moviename-" + "+".join(palabras_busqueda) + "/offset-" + str(pagina*40)
             try:
-                txtBusqueda = get(linkBusqueda, timeout=7).text
+                header = {"User-Agent": agents[pagina]}
+                browser_session = Session()
+                txtBusqueda = browser_session.get(linkBusqueda, timeout=7, headers=header).text
+                browser_session.close()
             except RequestException as e:
                 txtBusqueda = ''
 
             renglones = txtBusqueda.split("\n")
             indices = [x for x in range(len(renglones)) if " - Ver En l" in renglones[x]]
 
+            page_subs = []
             for indice in indices:
                 #Determinando enlace del subtítulo
                 enlace_0 = renglones[indice+1].index("href=") + 6
@@ -132,11 +83,11 @@ def opensubtitles(palabras):
                 else:
                     descripcion = ""
 
-                subs.append([fulltitle, descripcion, enlace])
+                page_subs.append([fulltitle, descripcion, enlace])
+            subs += page_subs
+            to_iterate = len(page_subs) > 0
 
             pagina += 1
-            #Tiempo de espera para evitar bloqueo
-            #time.sleep(1.5)
 
         return subs
 
