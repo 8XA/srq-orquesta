b78852f0dcbd5253acde00deb41f109de6a6ec26
8XA <ochoa.ssa@gmail.com>
2022-02-19 21:12:52 -0700

    Refactor: Branch detection in the update module

--- screens/update.py
+++ screens/update.py
@@ -8,24 +8,27 @@ from modules.admin_db import edit_settings, read_settings
 import sys
 
 def update():
-
     base_route = '/data/data/com.termux/files/usr/share/srq-orquesta/'
     absolute_route = base_route + 'srq-orquesta/'
     updates_route = base_route + 'update'
     data_backup_route = base_route + "data_backup.db"
 
+    branch_command = Popen(["git", "-C", base_route + "srq-orquesta", "status"], stdout=PIPE, stderr=PIPE)
+    out, error = branch_command.communicate()
+    branch = [branch for branch in ['master', 'develop'] if branch in str(out.splitlines()[0])][0]
+
     Popen("clear").wait()
     print("Verificando actualizaciones para SRQ ORQUESTA...\n")
 
-    remote = popen('cd '+ absolute_route + ' && git fetch origin develop ' + \
-            '&& git checkout remotes/origin/develop').read()
-    local = popen('cd ' + absolute_route + ' && git checkout develop').read()
+    remote = popen('cd '+ absolute_route + ' && git fetch origin ' + branch + \
+            ' && git checkout remotes/origin/' + branch).read()
+    local = popen('cd ' + absolute_route + ' && git checkout ' + branch).read()
     
     # Verifies if there is an update in the repositorie
     if "commit" in local:
         print("Actualizando script...\n")
         Popen(["rm", "-rf", updates_route], stdout=PIPE, stderr=PIPE)
-        clonar = system('git clone --branch develop --single-branch ' + \
+        clonar = system('git clone --branch ' + branch + ' --single-branch ' + \
                 'https://github.com/8XA/srq-orquesta.git ' + updates_route)
 
         # If the download was successful, it executes the update
