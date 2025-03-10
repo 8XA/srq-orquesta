cec4c7d96c6e7df1ede0dd6b00bf9460cbcd93be
8XA <69772360+8XA@users.noreply.github.com>
2025-03-09 19:35:03 -0700

    refactor: suggested_search uses the Google API instead of scraping Google

--- modules/suggested_search.py
+++ modules/suggested_search.py
@@ -1,34 +1,23 @@
 #!/bin/env python
 
+
 from requests import get
-from urllib.parse import unquote, quote
-from os import system
 
 def suggested_search(search):
-    """
-    It gets a sentence.
-    It returns the suggested search by google.
-    """
+    url = "https://suggestqueries.google.com/complete/search"
 
-    separated_search = search.split(' ')
-    for x in range(len(separated_search)):
-        separated_search[x] = quote(separated_search[x])
-    words_sum = '+'.join(separated_search)
+    params = {
+        "client": "firefox",
+        "q": search,
+        "hl": "es"
+    }
 
-    url = "https://www.google.com.mx/search?q=" + words_sum
-    try:
-        raw = get(url).text
-    except RequestException as e:
-        raw = ''
-
-    index_0 = raw.index('href="/search?q=')
-    index_0 = raw[index_0 + 1:].index('href="/search?q=') + index_0 + 17
-    index_1 = raw[index_0:].index('&amp;') + index_0
-
-    raw_suggested = raw[index_0:index_1]
-    suggested = " ".join(raw_suggested.split("+"))
-    suggested = unquote(suggested, encoding='utf-8', errors=' ')
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+    }
     
-    if len(suggested) == 0:
+    response = get(url, params=params, headers=headers)
+    if response.status_code == 200:
+        return response.json()[1][0]
+    else:
         return search
-    return suggested
