5e7c7cc4826a4e1267079acc4c31508a1342d2c3
8XA <69772360+8XA@users.noreply.github.com>
2025-03-13 16:50:36 -0700

    fix: opensubtitles scraper works with beautifulsoup4 and concurrency

--- modules/scrapers/subtitles/spanish/opensubtitles.py
+++ modules/scrapers/subtitles/spanish/opensubtitles.py
@@ -1,95 +1,115 @@
 #!/bin/env python
 
-#Recibe una lista de palabras de busqueda y retorna una
-#lista con los subtitulos encontrados en la página de
-#opensubtitles. El retorno tiene el formato:
-#[["titulo1", "descripcion1", "url1"], ["titulo2", "descripcion2", "url2"] ... ]
 
-import os
-from requests import get, Session
-from modules.rows_from_text_file import rows_from_text_file
-from requests.exceptions import RequestException
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from requests import Session
+from bs4 import BeautifulSoup
 from urllib.parse import quote
+import lxml
+from random import shuffle
+from itertools import cycle
 
-def opensubtitles(palabras):
-    try:
-        palabras_busqueda = [quote(palabra) for palabra in palabras]
-        agents = rows_from_text_file('user_agents.txt')
-        
-        pagina, subs = 0, []
-        to_iterate = True
-        #Solo descargará la primera página para evitar bloqueo
-        while to_iterate:
+from modules.rows_from_text_file import rows_from_text_file
 
-            linkBusqueda = "https://www.opensubtitles.org/es/search/sublanguageid-spa,spl/" + \
-                    "moviename-" + "+".join(palabras_busqueda) + "/offset-" + str(pagina*40)
-            try:
-                header = {"User-Agent": agents[pagina]}
-                browser_session = Session()
-                txtBusqueda = browser_session.get(linkBusqueda, timeout=7, headers=header).text
-                browser_session.close()
-            except RequestException as e:
-                txtBusqueda = ''
 
-            renglones = txtBusqueda.split("\n")
-            indices = [x for x in range(len(renglones)) if " - Ver En l" in renglones[x]]
+def opensubtitles(search_words):
+    """
+    This function take a sentence string as a parameter and returns a list of
+    subtitles from opensubtitles with the following format:
 
-            page_subs = []
-            for indice in indices:
-                #Determinando enlace del subtítulo
-                enlace_0 = renglones[indice+1].index("href=") + 6
-                enlace_1 = renglones[indice+1].index("onclick") - 2
-                enlace = "https://www.opensubtitles.org" + renglones[indice+1][enlace_0:enlace_1]
+    [["title1", "description1", "url1"], ["title2", "description2", "url2"] ... ]
+    """
 
-                #Si la descripcion aparece completa
-                if renglones[indice][0] == "(":
-                    indexador_0, indexador_1, corrector = "<br />", "<br /><a", 6
-                    desfase_t, desfase_a, desfase_d = 1, 0, 0
-                #Si la descripción aparece incompleta
-                else:
-                    indexador_0, indexador_1, corrector = "<span title=", '">', 13
-                    desfase_a, desfase_d = 0, 0
-                    while renglones[indice - desfase_a][0] != "(":
-                        desfase_a += 1
+    last_page = 0
+    agent = user_agent_generator()
+    scraping_lead = opensubtitles_onepage(search_words, last_page, next(agent))
 
-                    desfase_t = desfase_a + 1
-                    
-                    while "<span title=" not in renglones[indice - desfase_d]:
-                        desfase_d += 1
+    complete_subs_list = scraping_lead['subs_list']
+    executor = ThreadPoolExecutor(max_workers=20)
+    threads = dict()
 
-                #Determinando título del subtítulo
-                titulo_1_des = [x for x in renglones[indice-desfase_t]]
-                titulo_1_des.reverse()
-                titulo_0 = "".join(titulo_1_des).index('>"')
-                titulo = titulo_1_des[:titulo_0]
-                titulo.reverse()
-                titulo = "".join(titulo)
-                
-                #Determina el año
-                year_0 = renglones[indice-desfase_a].index("</a")
-                year = renglones[indice-desfase_a][:year_0]
+    while not scraping_lead['pagination_done'] or len(threads) != last_page:
+        for page in range(last_page + 1, scraping_lead['last_page']):
+            threads[page] = executor.submit(opensubtitles_onepage, search_words, page, next(agent))
 
-                #Título completo
-                fulltitle = titulo + " " + year
+            if page == scraping_lead['last_page'] - 1:
+                scraping_lead = threads[page].result()
+                last_page = page
 
-                #Determinando descripción del subtítulo
-                if (renglones[indice][0] == "(") or (renglones[indice-desfase_d][-3:] == "..."):
-                    descripcion_0 = renglones[indice-desfase_d].index(indexador_0) + corrector
-                    descripcion_1 = renglones[indice-desfase_d][descripcion_0:].index(indexador_1)
-                    descripcion = renglones[indice-desfase_d][descripcion_0:descripcion_1 + descripcion_0]
-                elif renglones[indice][0] not in "(<":
-                    descripcion = renglones[indice-1] + " " + \
-                            renglones[indice][:renglones[indice].index("<br /><a")]
-                else:
-                    descripcion = ""
+    for thread in as_completed(threads.values()):
+        complete_subs_list += thread.result()['subs_list']
 
-                page_subs.append([fulltitle, descripcion, enlace])
-            subs += page_subs
-            to_iterate = len(page_subs) > 0
+    executor.shutdown(wait=True)
 
-            pagina += 1
+    return complete_subs_list
 
-        return subs
 
-    except:
-        return []
+def opensubtitles_onepage(search_words, page, headers):
+    results = {
+        "subs_list": [],
+        "last_page": page,
+        "pagination_done": True
+    }
+
+    moviename = quote('+'.join(search_words.strip().split()))
+    url = (
+        f"https://www.opensubtitles.org/es/search/moviename-{moviename}"
+        f"/sublanguageid-spa,spl/offset-{page * 40}"
+    )
+
+    session = Session()
+    request = session.get(url, headers=headers)
+
+    if request.status_code != 200:
+        return results
+
+    soup = BeautifulSoup(str(request.text), "lxml")
+    raw_subs = soup.body.find_all(
+        'tr',
+        class_=lambda class_name: (
+            class_name and 'change' in class_name and 'expandable' in class_name
+        )
+    )
+
+    for raw_sub in raw_subs:
+        raw_description = str(raw_sub)
+
+        idx_1 = raw_description.find('<br/>')
+        idx_2 = raw_description[idx_1 + 5:].find('<br/>')
+        description = raw_description[idx_1 + 5:idx_1 + idx_2 + 5].strip()
+
+        if '<span title="' in description:
+            idx_1 = description.find('<span title="')
+            idx_2 = description[idx_1 + 13:].find('">')
+
+            description = f"{description[:idx_1]}{description[idx_1 + 13:idx_1 + 13 + idx_2]}"
+            description = description.replace("\n", "\t").split("\t")
+            description = '\n'.join([element for element in description if element != ''])
+
+        results['subs_list'].append([
+            raw_sub.find('a', class_='bnone')['title'][13:],
+            description,
+            f"https://dl.opensubtitles.org/en/download/sub/{raw_sub['id'][4:]}"
+        ])
+
+    pager = soup.find(id="pager")
+    if pager is None:
+        return results
+
+    pager = pager.find_all('a')
+    if len(pager) > 1:
+        if pager[-1].get_text() == '>>':
+            results['pagination_done'] = False
+            results['last_page'] = int(pager[-2].get_text())
+        else:
+            results['last_page'] = int(pager[-1].get_text())
+
+    return results
+
+
+def user_agent_generator():
+    agents = rows_from_text_file('user_agents.txt')
+    shuffle(agents)
+
+    for agent in cycle(agents):
+        yield {"User-Agent": agent}
