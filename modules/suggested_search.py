e0db0f6e5b9a8f1ec2e9e984bc2b0944809afb39
8XA <ochoa.ssa@gmail.com>
2025-03-15 13:15:15 -0700

    Refactor: Suggested search for Torrents works scraping Yahoo autocorrector instead of the Google API

--- modules/suggested_search.py
+++ modules/suggested_search.py
@@ -1,25 +1,29 @@
 #!/bin/env python
 
 
-from requests import get
+from requests import Session
+from bs4 import BeautifulSoup
+from random import sample
 from urllib.parse import quote
+
+from modules.rows_from_text_file import rows_from_text_file
 
 
 def suggested_search(search):
-    url = "https://suggestqueries.google.com/complete/search"
-
-    params = {
-        "client": "firefox",
-        "q": quote(search),
-        "hl": "es"
-    }
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+        "User-Agent": sample(rows_from_text_file('user_agents.txt'), 1)[0]
     }
-    
-    response = get(url, params=params, headers=headers)
-    if response.status_code == 200 and len(response.json()[1]) > 0:
-        return response.json()[1][0]
-    else:
-        return search
+
+    url = f"https://mx.search.yahoo.com/search?p={quote(search)}"
+
+    s = Session()
+    r = s.get(url, headers=headers)
+
+    if r.status_code == 200:
+        soup = BeautifulSoup(str(r.text), "lxml")
+        a_tags = soup.find_all('a', class_='fc-denim')
+        if len(a_tags) == 2:
+            return a_tags[0].get_text()
+
+    return search
