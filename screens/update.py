457762ca0178bc6423a4098ffb1ad35d9456e5bd
8XA <69772360+8XA@users.noreply.github.com>
2025-03-20 18:52:12 -0700

    Refactor: Actualizaciones basadas en versiones (tags de git)

--- screens/update.py
+++ screens/update.py
@@ -7,42 +7,56 @@ from modules.viewer import viewer
 from modules.admin_db import edit_settings, read_settings
 import sys
 
-def update():
-    base_route = '/data/data/com.termux/files/usr/share/srq-orquesta/'
-    absolute_route = base_route + 'srq-orquesta/'
-    updates_route = base_route + 'update'
-    data_backup_route = base_route + "data_backup.db"
 
-    branch_command = Popen(["git", "-C", base_route + "srq-orquesta", "status"], stdout=PIPE, stderr=PIPE)
-    out, error = branch_command.communicate()
-    branch = [branch for branch in ['master', 'develop'] if branch in str(out.splitlines()[0])][0]
+def update():
+    base_route = "/data/data/com.termux/files/usr/share/srq-orquesta/"
+    absolute_route = f"{base_route}srq-orquesta/"
+    updates_route = f"{base_route}update"
+    data_backup_route = f"{base_route}data_backup.db"
 
     Popen("clear").wait()
     print("Verificando actualizaciones para SRQ ORQUESTA...\n")
 
-    remote = popen('cd '+ absolute_route + ' && git fetch origin ' + branch + \
-            ' && git checkout remotes/origin/' + branch).read()
-    local = popen('cd ' + absolute_route + ' && git checkout ' + branch).read()
-    
-    # Verifies if there is an update in the repositorie
-    if "commit" in local:
+    versions_list = [tag[:-1] for tag in popen("git tag").readlines()]
+    current_version = popen("git describe --tags --exact-match").read()[:-1]
+
+    if current_version in versions_list and versions_list[-1] != current_version:
+        # Verifies if there is an update in the repositorie
         print("Actualizando script...\n")
         Popen(["rm", "-rf", updates_route], stdout=PIPE, stderr=PIPE)
-        clonar = system('git clone --branch ' + branch + ' --single-branch ' + \
-                'https://github.com/8XA/srq-orquesta.git ' + updates_route)
+
+        is_clonned = not system(
+            f"git clone --branch {versions_list[-1]} --single-branch "
+            "https://github.com/8XA/srq-orquesta.git {updates_route}"
+        )
 
         # If the download was successful, it executes the update
-        if clonar == 0:
+        if is_clonned:
             while read_settings('dimention_status') != 'stopped':
                 edit_settings("dimention_status", "stop")
                 sleep(0.5)
-            Popen(["cp", absolute_route + "data.db", data_backup_route], stdout=PIPE, stderr=PIPE).wait()
-            Popen(["rm", "-rf", absolute_route[:-1]], stdout=PIPE, stderr=PIPE).wait()
-            Popen(["mv", updates_route, absolute_route[:-1]], stdout=PIPE, stderr=PIPE).wait()
 
-            viewer("ACTUALIZACIÓN COMPLETA", "update")#
+            Popen(
+                ["cp", f"{absolute_route}data.db", data_backup_route],
+                stdout=PIPE,
+                stderr=PIPE
+            ).wait()
 
-            #Restart SRQ
+            Popen(
+                ["rm", "-rf", absolute_route[:-1]],
+                stdout=PIPE,
+                stderr=PIPE
+            ).wait()
+
+            Popen(
+                ["mv", updates_route, absolute_route[:-1]],
+                stdout=PIPE,
+                stderr=PIPE
+            ).wait()
+
+            viewer("ACTUALIZACIÓN COMPLETA", "update")
+
+            # Restart SRQ
             sys.stdout.flush()
             execv(sys.argv[0], sys.argv)
 
